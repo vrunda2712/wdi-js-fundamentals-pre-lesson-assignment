e24efccdc674d65037e2160c6afe677df932033d
vrunda2712 <angy.v27@gmail.com>
2017-06-28 17:07:22 -0400

    Added file for functions

---
+++ pacman-return.js
@@ -0,0 +1,29 @@
+function addScore(currentScore, eaten) {
+  switch (eaten) {
+  case 'dot':
+    scoreToAdd = 10;
+    break;
+  case 'powerPellet':
+    scoreToAdd = 50;
+    break;
+  case 'firstGhost':
+    scoreToAdd = 200;
+    break;
+  case 'secondGhost':
+    scoreToAdd = 400;
+    break;
+  }
+
+  return currentScore + scoreToAdd;
+}
+
+var score = 0;
+
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'powerPellet');
+score = addScore(score, 'firstGhost');
+score = addScore(score, 'secondGhost');
+
+console.log('Your score: ' + score);
