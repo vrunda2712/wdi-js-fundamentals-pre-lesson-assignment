b0982857f02dd69c74710172314e7cd12be202d6
vrunda2712 <angy.v27@gmail.com>
2017-06-28 16:57:59 -0400

    Added file for conditionals: If..else, switch

--- pacman.rb
+++ pacman.rb
@@ -10,12 +10,54 @@ puts ghosts[1]
 puts ghosts[2]
 puts ghosts[3]
 
+
+
 # Same as pacman-objects-1.js file
 
 class Ghost
   attr_accessor :colour, :personality
 end
+
 inky = Ghost.new
 inky.colour = 'Cyan'
 inky.personality = 'Shadow'
 puts inky.inspect
+
+
+
+# Same as pacman-if-else.js file
+
+power_pellet_eaten = false
+ghosts = 4
+while true do
+  puts "Ghosts remaining: #{ghosts}"
+
+  if power_pellet_eaten == false
+    puts 'Pac-Man eats the power pellet.'
+    power_pellet_eaten = true
+  elsif ghosts > 0
+    puts 'Pac-Man eats a ghost.'
+    ghosts--
+  else
+    break
+  end
+end
+
+puts 'Pac-Man goes home after a long day in the maze.'
+
+
+
+# Same as pacman-switch.js file
+
+ghost = 'Pinky'
+case ghost
+when 'Inky'
+  colour = 'Cyan'
+when 'Blinky'
+  colour = 'Red'
+when 'Pinky'
+  colour = 'Pink'
+when 'Clyde'
+  colour = 'Orange'
+end
+puts "#{ghost} is the colour #{colour}"
